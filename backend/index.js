accd9b12f4fa05eb9e36ad5ee398f7f866a215cb
girishSaiWork <girishsai.thiruvidhula.work@gmail.com>
2024-09-10 10:04:06 -0400

    PORT env

--- backend/index.js
+++ backend/index.js
@@ -3,18 +3,19 @@ import express from "express"
 import { client } from "./db/connectDB.js"
 import authRoutes from "./routes/auth_route.js"
 
-
-const app = express()
 dotenv.config()
+const app = express()
+const PORT = process.env.PORT || 6000
 
-app.get("/", (req, res) => {
-    res.send("Hi... Finally working on Java Script and web dev")
-})
+
+// app.get("/", (req, res) => {
+//     res.send("Hi... Finally working on Java Script and web dev")
+// })
 
 
 app.use("/app/auth", authRoutes)
 
-app.listen(3000, () => {
+app.listen(PORT, () => {
     client();
-    console.log("Server is running.....")
+    console.log("Server is running on port : ", PORT)
 });
